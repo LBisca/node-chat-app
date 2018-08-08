157daac45c0ef748c50d8928aad2bbd7432ae39e
LBisca <lucas.simon.b@gmail.com>
2018-08-08 16:27:55 -0300

    Change Port

--- server/server.js
+++ server/server.js
@@ -2,10 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const publicPath = path.join(__dirname, '../public')
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 var app = express()
 
-console.log(__dirname + '/../public')
 console.log(publicPath)
 
 app.use(express.static(publicPath))
