f674b11d1355e12894f0f7becac38f104edaec58
LBisca <lucas.simon.b@gmail.com>
2018-08-10 13:36:48 -0300

    Create text form for createMessage

--- server/server.js
+++ server/server.js
@@ -10,26 +10,31 @@ var app = express()
 var server = http.createServer(app)
 var io = socketIO(server)
 
-console.log(publicPath)
-
 app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
-    console.log('New user connected')
+  console.log('New user connected')
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
 
-    socket.on('createMessage', (message, callback) => {
-        console.log('message', message)
-        io.emit('newMessage', generateMessage(message.from. message.text))
-        callback()
-    })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-    socket.on('disconnect', () => {
-        console.log("User was disconnected")
-    })
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message)
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.')
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected')
+  })
 })
 
 server.listen(port, () => {
-    console.log(`Server started at port ${port}`)
+  console.log(`Server is up on ${port}`);
 })
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -3,12 +3,12 @@ var expect = require('expect')
 var {generateMessage} = require('./message')
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
-        var from = 'Jen'
-        var text = 'some message'
-        var message = generateMessage(from, text)
+  it('should generate correct message object', () => {
+    var from = 'Jen'
+    var text = 'Some message'
+    var message = generateMessage(from, text)
 
-        expect(message.createdAt).toBeA('number')
-        expect(message).toInclude({from, text})
-    })
+    expect(message.createdAt).toBeA('number')
+    expect(message).toInclude({from, text})
+  })
 })
