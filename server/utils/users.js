0bd6467e52a08328957d53db1193d0af1ff09b7b
LBisca <lucas.simon.b@gmail.com>
2018-08-20 12:54:19 -0300

    Add more tests

--- server/utils/users.js
+++ server/utils/users.js
@@ -15,6 +15,27 @@ class Users {
         return user
     }
 
+    getUser (id) {
+        return this.users.filter((user) => user.id === id)[0]
+     }
+
+    removeUser (id) { 
+        var user = this.getUser(id)
+
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id)
+        }
+
+        return user
+    }
+
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room)
+        var nameArray = users.map((user) => user.name)
+
+        return nameArray
+    }
+
 }
 
 module.exports = {Users}
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -1,8 +1,26 @@
 const expect = require('expect')
 
-const {Users} = require('./users')
+const { Users } = require('./users')
 
 describe('Users', () => {
+    var users
+
+    beforeEach(() => {
+        users = new Users()
+        users.users = [{
+            id: '1',
+            name: 'Robert',
+            room: 'Node Course'
+        }, {
+            id: '2',
+            name: 'Lucas',
+            room: 'Nutella'
+        }, {
+            id: '3',
+            name: 'Carlos',
+            room: 'Node Course'
+        }]
+    })
 
     it('Should add a new User', () => {
         var users = new Users()
@@ -14,5 +32,47 @@ describe('Users', () => {
         var resUser = users.addUser(user.id, user.name, user.room)
 
         expect(users.users).toEqual([user])
+    })
+
+    it('Should remove a user', () => {
+        var userId = '1'
+        var user = users.removeUser(userId)
+
+        expect(user.id).toBe(userId)
+        expect(users.users.length).toBe(2)
+    })
+
+    it('Should not remove a user', () => {
+        var userId = '99'
+        var user = users.removeUser(userId)
+
+        expect(user).toNotExist
+        expect(users.users.length).toBe(3)
+    })
+
+    it('Should find user', () => {
+        var userId = '2'
+        var user = users.getUser(userId)
+
+        expect(user.id).toBe(userId)
+    })
+
+    it('Should not find user', () => {
+        var userId = '99'
+        var user = users.getUser(userId)
+
+        expect(user).toNotExist()
+    })
+
+    it('Should return names for Node Course', () => {
+        var userList = users.getUserList('Node Course')
+
+        expect(userList).toEqual(['Robert', 'Carlos'])
+    })
+
+    it('Should return names for Nutella', () => {
+        var userList = users.getUserList('Nutella')
+
+        expect(userList).toEqual(['Lucas'])
     })
 })
